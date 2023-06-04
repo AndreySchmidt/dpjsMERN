9a084470cefdd6c0bb05eb1ffb463331420b579d
AndreySchmidt <ringnull@gmail.com>
2023-06-04 12:32:42 +0300

    index

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   return res.json({ message: "Home" });
+// });
+
+async function start() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.tqzbwkk.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+    );
+
+    app.listen(PORT, () => {
+      console.log("aaa");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
